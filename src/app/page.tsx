8f62f939bb8a3bffdfbe65ef1f55dfb515fe8ac4
Ana <anaclimgo@gmail.com>
2025-11-09 19:24:22 -0300

    feat: added tech stack to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero"
 import { ThemeToggle } from "@/components/ThemeToggle"
+import TechStack from "@/components/TechStack"
 
 export default function Home() {
   return (
     <main className="relative">
       <ThemeToggle />
       <Hero />
+      <TechStack />
     </main>
   )
 }
