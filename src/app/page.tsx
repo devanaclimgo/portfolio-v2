53722d5d7eaba34915d2fad9e65020c792604abc
Ana <anaclimgo@gmail.com>
2025-11-09 20:08:13 -0300

    feat: added projects component to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Hero from "@/components/Hero"
 import { ThemeToggle } from "@/components/ThemeToggle"
 import TechStack from "@/components/TechStack"
 import About from "@/components/About"
+import { Projects } from "@/components/Projects"
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <TechStack />
       <About />
+      <Projects />
     </main>
   )
 }
